1e5f8d098372458ccfae77fa7ac7d35a6b804073
Umberto Ixxo <sfumato1977@gmail.com>
2014-10-26 12:32:15 +0100

    modified:   Mediacom101S2_def.config 	deleted:    Module.symvers 	deleted:    drivers/video/logo/tchip_sysInf.c 	deleted:    include/config/auto.conf 	deleted:    include/config/tristate.conf 	deleted:    include/generated/autoconf.h 	deleted:    include/generated/compile.h

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 5 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 SMP PREEMPT Fri Oct 3 02:27:46 CEST 2014"
-#define LINUX_COMPILE_BY "spin877"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILER "gcc version 4.8.2 (Ubuntu/Linaro 4.8.2-16ubuntu4) "
